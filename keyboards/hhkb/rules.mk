2162ea61444d7bf0061c6752cc67be085920acee
Joshua Huber <jhuber@blockbridge.com>
2017-06-11 01:21:49 -0400

    Add rn42 support from tmk hhkb repo
    This is almost certainly not integrated into qmk correctly, as I'm not familiar
    with the source at all. But this *does* seem to work, at least for my initial
    testing.

--- keyboards/hhkb/rules.mk
+++ keyboards/hhkb/rules.mk
@@ -55,6 +55,36 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 # as per original hasu settings
 OPT_DEFS += -DBOOTLOADER_SIZE=4096
 
+# XXX: enable support for hasu's BT alt controller -- code borrowed from tmk source tree.
+
+# either uncomment the HHKB_RN42_ENABLE line below, or run make enabling the
+# feature. Be sure to clean any existing build before trying to enable rn42
+# support. For example:
+#
+# make hhkb-keymap-clean
+# make hhkb-keymap-dfu HHKB_RN42_ENABLE=yes
+
+#HHKB_RN42_ENABLE = yes
+
+ifeq ($(strip $(HHKB_RN42_ENABLE)), yes)
+
+OPT_DEFS += -DHHKB_RN42_ENABLE
+
+# Support for the RN42 Bluetooth module. This is the BT module in Hasu's BT
+# HHKB Alt controller.
+RN42_DIR = rn42
+
+SRC +=  serial_uart.c \
+	rn42/suart.S \
+	rn42/rn42.c \
+	rn42/rn42_task.c \
+	rn42/battery.c \
+	rn42/main.c
+
+VPATH += $(RN42_DIR)
+
+endif
+
 # Build Options
 #   comment out to disable the options.
 #
